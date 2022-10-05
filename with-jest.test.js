99d5d47c280541cad0f0b8948561c869063951bf
johannes-vogel <31311694+johannes-vogel@users.noreply.github.com>
2022-10-05 19:39:09 +0200

    add files

---
+++ with-jest.test.js
@@ -0,0 +1,22 @@
+test('xxx', (done) => {
+  const express = require('express')
+  const app = express()
+  
+  app.get('/test', (req, res) => {
+    console.log('request received')
+    res.set('x-header', 'value')
+    res.send('ok')
+  })
+  
+  const server = app.listen(4004, async () => {
+    const a = require('axios')
+    const axios = a.create({
+      headers: { 'Content-Type': 'application/json' },
+      baseURL: 'http://localhost:4004'
+    })
+    let response = await axios.get('/test')
+    console.log(response.headers)
+    server.close()
+    done()
+  })
+})
